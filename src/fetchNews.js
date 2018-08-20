a22f27ed3d9a4aba0d4974a2470eda629c8ed223
Osama Miro <o.miro650@gmail.com>
2018-08-20 16:30:05 -0400

    news5

--- src/fetchNews.js
+++ src/fetchNews.js
@@ -5,16 +5,24 @@ import {TweenMax, Power1, TimelineLite} from "gsap";
 import GSAP from 'react-gsap-enhancer'
 import TransitionGroup from 'react-addons-transition-group';
 import NewsBubble from "./newsBubble.js"
+
 class Fetch extends React.Component {
 state = {
   news: '',
   clicked: false,
   clickedBubble: false,
   newsAJ:'',
+  newsG: '',
+  newsDN: '',
 }
 
 componentDidMount(){
-  let ke = 0
+  let ke = 'ha'
+  let ytk = 'ha'
+  let dn = "ha"
+  let d = `ha`
+
+
 fetch(`https://newsapi.org/v2/top-headlines?sources=vice-news&apiKey=${ke}`)
 .then(res => res.json())
 .then(json => {
@@ -29,7 +37,27 @@ fetch(`https://newsapi.org/v2/top-headlines?sources=al-jazeera-english&apiKey=${
     newsAJ: json
   })
 })
+
+fetch(`https://newsapi.org/v2/top-headlines?sources=the-guardian-uk&apiKey=${ke}`)
+.then(res => res.json())
+.then(json => {
+  this.setState({
+    newsG: json
+  })
+})
+
+
+//GET https://www.googleapis.com/youtube/v3/channels?part=contentDetails&forUsername=democracynow&key={YOUR_API_KEY}
+fetch(`https://www.googleapis.com/youtube/v3/playlistItems?part=snippet%2CcontentDetails%2Cstatus&playlistId=${d}&key=${ytk}`)
+        .then(res => res.json())
+        .then(json => {
+        this.setState({
+          newsDN: json
+        })
+  })
+
 }
+// json.items[0].contentDetails.videoId
 
 handleClick = () => {
 // console.log(this.state.news)
@@ -60,6 +88,23 @@ return <div className="article1">
 </div>
 }
 
+showGNews = () => {
+console.log(this.state.newsG)
+return <div className="article2">
+<h5 className="text" >{this.state.newsG.articles[0].source.name}</h5>
+<img className="img" src={this.state.newsG.articles[0].urlToImage} height="100%" width="100%" />
+<h5 className="text">{this.state.newsG.articles[0].title}</h5>
+<a className="text" target="_blank" rel="noopener noreferrer" href={this.state.newsG.articles[0].url}>Read</a>
+</div>
+}
+
+showDNnews = () => {
+  console.log(this.state.newsDN.items[0])
+  let video = this.state.newsDN.items[0].contentDetails.videoId
+  console.log(video)
+  return <ReactPlayer className="video" url={`https://www.youtube.com/watch?v=${video}`} playing />
+}
+
   render () {
     let background = {one: "news"}
     console.log("g")
@@ -71,6 +116,8 @@ return <div className="article1">
       <div onClick={this.handleClick} className={background.one} ref={c => this.container = c}/>
       {this.state.clicked ? this.showViceNews() : null}
       {this.state.clicked ? this.showAJNews() : null}
+      {this.state.clicked ? this.showGNews() : null}
+      {this.state.clicked ? this.showDNnews() : null}
       </div>
     )
   }
